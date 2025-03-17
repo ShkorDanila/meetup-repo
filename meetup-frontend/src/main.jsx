f1bba530f7b9c4cc4e3490dcd9cb8b734cab06d0
ShkorDanila <89966813+ShkorDanila@users.noreply.github.com>
2025-03-17 23:03:00 +0300

    add routing skeleton

--- meetup-frontend/src/main.jsx
+++ meetup-frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AuthProvider from "./providers/AuthProvider.jsx";
+import ApiProvider from "./providers/ApiProvider.jsx";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import { CookiesProvider } from "react-cookie";
 
-createRoot(document.getElementById('root')).render(
+const router = createRouter({ routeTree });
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CookiesProvider>
+      <AuthProvider>
+        <ApiProvider>
+          <RouterProvider router={router} />
+        </ApiProvider>
+      </AuthProvider>
+    </CookiesProvider>
+  </StrictMode>
+);
--- meetup-frontend/src/providers/AuthProvider.jsx
+++ meetup-frontend/src/providers/AuthProvider.jsx
@@ -1,3 +1,9 @@
-const AuthProvider = () => {
-    
-}
+import { useCookies } from "react-cookie";
+
+const AuthProvider = ({ children }) => {
+  const [cookies, setCookie, removeCookie] = useCookies(["auth-token"]);
+
+  return <>{children}</>;
+};
+
+export default AuthProvider;
